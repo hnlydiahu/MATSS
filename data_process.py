ea59e8bea0598a181bbc67db0707ddfa578d6704
yhu <lydiahu_hn@hotmail.com>
2022-10-09 22:15:58 +1100

    update at 09/10/2022

--- data_process.py
+++ data_process.py
@@ -1,7 +1,16 @@
 import numpy as np
 import pandas as pd
 
-
+topics = {}
+topic_network = []
 df = pd.read_csv('dataset/movies.csv', sep='::', header=None)
 for i in range(len(df[2])):
-    print(df[2][i])
+    types = df[2][i].split('|')
+    for n in range(len((types))):
+        if topics.get(types[n]) is None:
+            topics[types[n]] = 1
+        else:
+            value = topics[types[n]]
+            value += 1
+            topics[types[n]] = value
+print(topics)
